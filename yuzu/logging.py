254e5e59ec60c9629c26bfec6c5a62a6ce4ab9fa
Yasunori Horikoshi <hotoku@users.noreply.github.com>
2021-09-28 16:14:48 +0900

    aes

--- yuzu/logging.py
+++ yuzu/logging.py
@@ -47,10 +47,12 @@ def get_annotation(LOGGER):
             def executor(*args, **kw):
                 args_ = [a for i, a in enumerate(args) if i not in ignore_args]
                 kw_ = {k: v for k, v in kw.items() if k not in ignore_kw}
-                LOGGER.info(f"{f.__name__} {args_} {kw_} start (pid={os.getpid()})")
+                LOGGER.info(
+                    f"{f.__name__} {args_} {kw_} start (pid={os.getpid()})")
                 ret = f(*args, **kw)
                 if return_value:
-                    LOGGER.info(f"{f.__name__} end (pid={os.getpid()}), value={ret}")
+                    LOGGER.info(
+                        f"{f.__name__} end (pid={os.getpid()}), value={ret}")
                 else:
                     LOGGER.info(f"{f.__name__} end (pid={os.getpid()})")
                 return ret
